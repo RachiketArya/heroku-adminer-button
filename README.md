7153d58471ea9fa5bb7a30580cdddda298471e40
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2020-07-09 00:28:08 -0400

    reuse plugin, update readme

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Web Administrative tool from https://www.adminer.org
 CTRL (or CMD 💻) + Click to start
 
 [![Heroku Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/alexsorokoletov/heroku-adminer-button)
+
+
+### Customization in Heroku
+
+This setup supports:
+ADMINER_BASIC_USER -  HTTP BASIC user name
+ADMINER_BASIC_PASS_MD5 -  HTTP BASIC password (md5 of it)
+ADMINER_DATABASE_URL1 - preconfigured db connection string (for example PostgresSQL database url)
+ADMINER_DATABASE_NAME1 - preconfigured server friendly name
--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php
 
-$valid_passwords = array (getenv('CUSTOMIZE_BASIC_USER') => getenv('CUSTOMIZE_BASIC_AUTH'));
+$valid_passwords = array (getenv('ADMINER_BASIC_USER') => getenv('CUSTOMIZE_BASIC_PASS_MD5'));
 $valid_users = array_keys($valid_passwords);
 
 $user = $_SERVER['PHP_AUTH_USER'];
 $pass = $_SERVER['PHP_AUTH_PW'];
 
-$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+$validated = (in_array($user, $valid_users)) && ($pass == md5($valid_passwords[$user]));
 
 if (!$validated) {
   header('WWW-Authenticate: Basic realm="Adminer Web UI"');
   header('HTTP/1.0 401 Unauthorized');
   die ("Not authorized");
 }
-
-
 
 function adminer_object() {
     // required to run any plugin
@@ -30,37 +28,15 @@ function adminer_object() {
          new AdminerEditForeign,
          new AdminerTablesFilter,
          new AdminerEnumTypes,
-         new AdminerEnumOption
+         new AdminerEnumOption,
+         new AdminerLoginServers([
+            getenv('ADMINER_DATABASE_URL1') => getenv('ADMINER_DATABASE_NAME1'),
+        ]),
     );
     
     class AdminerCustomization extends AdminerPlugin {
         function name() {
             return "Adminer - Data Entry";
-        }
-
-        /** Print login form
-        * @return null
-        */
-        function loginForm() {
-            global $drivers;
-            $fixedDriver = getenv('CUSTOMIZE_DATABASE_DRIVER');
-            echo "<!-- driver is " . h($fixedDriver);
-            var_dump($drivers);
-            echo "-->";
-            $dbServer = getenv('CUSTOMIZE_DATABASE_SERVER');
-            echo "<table cellspacing='0' class='layout'>\n";
-            echo $this->loginFormField('driver', '<tr><th>' . lang('System') . '<td>', html_select("auth[driver]", $drivers, DRIVER, "loginDriver(this);") . "\n");
-            echo $this->loginFormField('server', '<tr><th>' . lang('Server') . '<td>', '<input type="password" name="auth[server]" value="' . h($dbServer) . '" title="hostname[:port]" placeholder="..." autocapitalize="off">' . "\n");
-            echo $this->loginFormField('username', '<tr><th>' . lang('Username') . '<td>', '<input name="auth[username]" id="username" value="' . h($_GET["username"]) . '" autocomplete="username" autocapitalize="off">' . script("focus(qs('#username')); qs('#username').form['auth[driver]'].onchange();"));
-            echo $this->loginFormField('password', '<tr><th>' . lang('Password') . '<td>', '<input type="password" name="auth[password]" autocomplete="current-password">' . "\n");
-            echo $this->loginFormField('db', '<tr><th>' . lang('Database') . '<td>', '<input name="auth[db]" value="' . h($_GET["db"]) . '" autocapitalize="off">' . "\n");
-            echo "</table>\n";
-            echo "<p><input type='submit' value='" . lang('Login') . "'>\n";
-            echo checkbox("auth[permanent]", 1, $_COOKIE["adminer_permanent"], lang('Permanent login')) . "\n";
-        }
-        function database() {
-           // database name, will be escaped by Adminer
-           return DB;
         }
     }
     return new AdminerCustomization($plugins);
